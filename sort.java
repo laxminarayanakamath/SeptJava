5bd50c65272a1cc834b8d80a48c4952412578c86
laxminarayan <laxminarayan.kamath@codecraft.co.in>
2023-09-05 16:41:07 +0530

    bubble sort

---
+++ sort.java
@@ -0,0 +1,35 @@
+
+public class sort {
+    public static void main(String[] args) {
+        int[] array = {64, 34, 25, 12, 22, 11, 90};
+
+        System.out.println("Original array:");
+        printArray(array);
+
+        sort(array);
+
+        System.out.println("\nSorted array:");
+        printArray(array);
+    }
+
+    public static void sort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
